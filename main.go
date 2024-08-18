eb2da4d527c47b0a04770f2da1fd4dada4ed8ad8
Austin <austin1habba@gmail.com>
2024-08-18 14:00:29 -0500

    More detailed output in main example

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/lazarus/arithmetic-coding-go/ac"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
@@ -15,14 +16,20 @@ func main() {
 	}
 
 	context := ac.NewContext()
+	start := time.Now()
 	encoded := context.Encode(data)
+	elapsed := time.Since(start)
 
-	fmt.Printf("Original size: %d bytes, encoded size: %d bytes\n", len(data), len(encoded))
+	reductionPercentage := float64(len(data)-len(encoded)) / float64(len(data)) * 100
+
+	fmt.Printf("Original size: %d bytes, encoded size: %d bytes, a reduction of %.2f%% in %v!\n", len(data), len(encoded), reductionPercentage, elapsed.String())
 
 	context = ac.NewContext()
+	start = time.Now()
 	decoded := context.Decode(encoded)
+	elapsed = time.Since(start)
 
-	fmt.Printf("Encoded size: %d bytes, decoded size: %d bytes\n", len(encoded), len(decoded))
+	fmt.Printf("Encoded size: %d bytes, decoded size: %d bytes in %v\n", len(encoded), len(decoded), elapsed.String())
 
 	if string(data) == string(decoded) {
 		fmt.Println("Success: The original and decoded data match!")
